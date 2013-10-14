a872ce6b35cf0087df16d2f1d1fba99ae5717393
emilknievel <emil.vagstedt@gmail.com>
2013-10-14 10:51:39 +0200

    Indentation fix.
    Added: testord.txt that contains test cases for the program.

--- src/Queue.java
+++ src/Queue.java
@@ -2,32 +2,32 @@
 
 class ListNode
 {
-  Object element;
-  ListNode next = null;
+    Object element;
+    ListNode next = null;
 }
 
 class Queue
 {
-  private ListNode front = null, back = null;
+    private ListNode front = null, back = null;
 
-  public void Put(Object element)
-  {
-    if (IsEmpty()) back = front = new ListNode();
-    else back = back.next = new ListNode();
-    back.element = element;
-  }
+    public void Put(Object element)
+    {
+	if (IsEmpty()) back = front = new ListNode();
+	else back = back.next = new ListNode();
+	back.element = element;
+    }
 
-  public Object Get() throws Exception
-  {
-    if (IsEmpty()) throw new Exception();
-    Object element = front.element;
-    front = front.next;
-    return element;
-  }
+    public Object Get() throws Exception
+    {
+	if (IsEmpty()) throw new Exception();
+	Object element = front.element;
+	front = front.next;
+	return element;
+    }
 
-  public boolean IsEmpty()
-  { return front == null; }
+    public boolean IsEmpty()
+    { return front == null; }
 
-  public void Empty()
-  { front = back = null; }
+    public void Empty()
+    { front = back = null; }
 }
